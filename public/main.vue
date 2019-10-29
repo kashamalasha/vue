ac5c29cca211acb49ffac43c7127d8b1a6e992a2
d.burnyshev <d.burnyshev@tinkoff.ru>
2019-10-29 17:51:41 +0300

    Firebase init

---
+++ public/main.vue
@@ -0,0 +1,46 @@
+Vue.component('product',{
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+    <div>
+      <h1>{{ title }}</h1>
+      <button v-on:click="pushMe">Push Me! = {{ count }}</button>
+      <p>Shipping: {{ shipping }}</p>
+    </div>
+  `,
+  data() {
+    return {
+      product: 'Test',
+      brand: 'My',
+      count: 0
+    }
+  },
+  methods: {
+    pushMe() {
+      this.count++;
+    }
+  },
+  computed: {
+    title() {
+      return this.product + ' ' + this.brand
+    },
+    shipping() {
+      if (this.premium) {
+        return 'Free'
+      }
+      return '2.99'
+    }
+  }
+})
+
+
+var app = new Vue ({
+  el: '#app',
+  data: {
+    premium: true
+  }
+})
